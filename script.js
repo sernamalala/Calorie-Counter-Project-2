7b2c4d6b6f85e202bc9df2311a3dec8c3be61b53
sernamalala <sernaprograms@gmail.com>
2023-12-24 17:36:00 +0200

    added basic javaScript variabled to the project

---
+++ script.js
@@ -0,0 +1,14 @@
+let calorieCounter = document.getElementById("calorie-counter");
+let budgetNumberInput = document.getElementById("budget");
+let entryDropdown = document.getElementById("entry-dropdown");
+
+let addEntryButton = document.getElementById("add-entry");
+let clearButton = document.getElementById("clear");
+let output = document.getElementById("output");
+
+let isError = false;
+
+function cleanInputString(str){
+
+    const strArray = str.split("");
+}
